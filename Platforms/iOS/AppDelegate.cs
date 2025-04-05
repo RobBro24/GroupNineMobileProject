f478534d37d3e194fc9673f43c4bff5550069505
Grey <gray.andrews.ga@gmail.com>
2025-04-04 20:07:00 -0400

    Intial .net maui app template creation

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GroupNineMobileProject
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
